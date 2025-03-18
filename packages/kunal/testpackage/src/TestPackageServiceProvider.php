a887d16f2a09fa4e9b2995682b1b1152ff27a364
Kunal <kunalkr0007@gmail.com>
2025-03-18 14:08:18 +0530

    Initial Commit

---
+++ packages/kunal/testpackage/src/TestPackageServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace YourVendor\TestPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestPackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Load the package routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        
+        // Load the package views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'testpackage');
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register package services if any
+    }
+}
